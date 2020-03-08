52b75d789f93556c501a463e66b6926b0f14861c
VishnuuVijayan <vishnu08ptb@gmail.com>
2020-03-08 20:23:32 +0530

    Update homescreen.component.js

--- src/components/homescreen.component.js
+++ src/components/homescreen.component.js
@@ -1,95 +1,104 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, { Component, useStyles } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import axios from "axios";
 
-export default class Home extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { disasters: [], images: [] };
+export default function HomeScreen() {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { disasters: [], images: [] };
+  // }
+  // componentDidMount() {
+  //   this.getData();
+  // }
+
+  // getData = () => {
+  //   axios
+  //     .get("http://localhost:5000/disaster/")
+  //     .then(response => {
+  //       const data = response.data;
+  //       this.setState({ disasters: data });
+  //       console.log("Data has been Recieved");
+  //     })
+  //     .catch(() => {
+  //       alert("Error Recieving Data");
+  //     });
+  // };
+
+  // render() {
+  const useStyles = makeStyles(theme => ({
+    icon: {
+      marginRight: theme.spacing(2)
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6)
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4)
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8)
+    },
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column"
+    },
+    cardMedia: {
+      paddingTop: "56.25%" // 16:9
+    },
+    cardContent: {
+      flexGrow: 1
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6)
     }
-    componentDidMount() {
-      this.getData();
-    }
-  
-    getData = () => {
-      axios
-        .get("http://localhost:5000/disaster/")
-        .then(response => {
-          const data = response.data;
-          this.setState({ disasters: data });
-          console.log("Data has been Recieved");
-        })
-        .catch(() => {
-          alert("Error Recieving Data");
-        });
-    };
-
-const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export default function Album() {
+  }));
   const classes = useStyles();
-
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <React.Fragment>
       <CssBaseline />
-    
+
       <main>
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-Carosel            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Carosel{" "}
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
               Carosel images
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary">
-Go TO                  </Button>
+                    Go TO{" "}
+                  </Button>
                 </Grid>
-                <Grid item>
-                </Grid>
+                <Grid item></Grid>
               </Grid>
             </div>
           </Container>
@@ -109,14 +118,12 @@ Go TO                  </Button>
                     <Typography gutterBottom variant="h5" component="h2">
                       Heading
                     </Typography>
-                    <Typography>
-Contents                    </Typography>
+                    <Typography>Contents </Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small" color="primary">
                       View
                     </Button>
-                    
                   </CardActions>
                 </Card>
               </Grid>
@@ -129,11 +136,18 @@ Contents                    </Typography>
         <Typography variant="h6" align="center" gutterBottom>
           Footer Here
         </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-Team Vibe        </Typography>
-        <Copyright />
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Team Vibe{" "}
+        </Typography>
+        {/* <Copyright /> */}
       </footer>
       {/* End footer */}
     </React.Fragment>
   );
+  // }
 }
