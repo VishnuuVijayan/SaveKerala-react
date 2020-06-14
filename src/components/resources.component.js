5a6da28168faa46f3d5a29fb4a5be8418e5665c4
Shahnaspv01 <36195132+Shahnaspv01@users.noreply.github.com>
2020-06-14 22:14:32 +0530

    .

--- src/components/resources.component.js
+++ src/components/resources.component.js
@@ -117,8 +117,9 @@ export default class Resources extends React.Component {
         <input type="checkbox" value="Female" name="gender" /> Food <br></br>
         <input type="checkbox" value="Other" name="gender" /> Clothing <br></br>
         <input type="checkbox" value="Other" name="gender" /> Medicine <br></br>
-        <input type="checkbox" value="Other" name="gender" /> Clothing <br></br>
         <input type="checkbox" value="Other" name="gender" /> Kitchen utensil <br></br>
+        <input type="checkbox" value="Other" name="gender" /> other <br></br>
+
       </div>
       
       </div>
